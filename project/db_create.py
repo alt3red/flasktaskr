b4deedc2705c3fa2eee70fd582b89929f3782fec
Raz Damaschin <razdjp@gmail.com>
2018-01-11 07:54:00 -0600

    switched to SQLAlchemy

--- project/db_create.py
+++ project/db_create.py
@@ -1,10 +1,10 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    c = connection.cursor()
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name TEXT NOT NULL, due_date TEXT NOT NULL,
-                  priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
-    c.execute('INSERT INTO tasks(name, due_date, priority, status) VALUES ("Finish this tutorial", "01/25/18", 10, 1)')
-    c.execute('INSERT INTO tasks(name, due_date, priority, status) VALUES ("Finish RP Course 2", "02/01/18", 10, 1)')
+db.create_all()
+
+db.session.add(Task("Finish this tutorial", date(2018, 1, 12), 10, 1))
+db.session.add(Task("Finish Real Python", date(2018, 2, 1), 10, 1))
+
+db.session.commit()
